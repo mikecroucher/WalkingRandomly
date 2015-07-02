8cde284c61819e8377f32e9aafaa84ceebe851c4
Amro <amroamroamro@gmail.com>
2015-07-02 19:44:36 +0300

    improve testing by using timeit instead of tic/toc
    Rewrite each solution as a separate function, that way testing is more
    fair and accurate.
    
    Also add two other implementations; one using HANKEL, the other
    is a C++ implementation for a MEX-function.

--- vectorisation_example.m
+++ vectorisation_example.m
@@ -1,81 +1,105 @@
 % This is a demonstration script for the blog post:
 % http://www.walkingrandomly.com/?p=5774
 
-clear all;
-N=2000;
+function varargout = vectorisation_example(N)
+    % matrix size
+    if nargin < 1, N = 2000; end
 
-%Generate a N-by-N matrix where A(i,j) = i + j;
-tic
-for ii = 1:N
-    for jj = 1:N
-        Aloop(ii,jj) = ii + jj;
+    % functions to test
+    funcs = {...
+        @() func1_loop_noprealloc_rowmajor(N) ;
+        @() func2_loop_prealloc_rowmajor(N)   ;
+        @() func3_loop_prealloc_colmajor(N)   ;
+        @() func4_vectorized_meshgrid(N)      ;
+        @() func5_vectorized_matmult(N)       ;
+        @() func6_vectorized_repmat(N)        ;
+        @() func7_vectorized_cumsum(N)        ;
+        @() func8_vectorized_bsxfun(N)        ;
+        @() func9_vectorized_hankel(N)        ;
+        @() func10_mex_c(N)
+    };
+
+    % timeit
+    t = cellfun(@timeit, funcs, 'UniformOutput',true);
+    fprintf('N = %d\n', N);
+    for i=1:numel(t)
+        fprintf('%35s = %f seconds.\n', ...
+            strrep(func2str(funcs{i}), '@()',''), t(i));
+    end
+
+    % check equal results
+    v = cellfun(@feval, funcs, 'UniformOutput',false);
+    assert(isequal(v{:}), 'Results are not equal');
+
+    % return output if requested
+    if nargout > 1, varargout{1} = t; end
+    if nargout > 2, varargout{2} = v; end
+end
+
+
+function A = func1_loop_noprealloc_rowmajor(N)
+    % Generate a N-by-N matrix where A(i,j) = i + j;
+    for ii = 1:N
+        for jj = 1:N
+            A(ii,jj) = ii + jj; %#ok<AGROW>
+        end
     end
 end
-original = toc;
 
-%preallocate
-tic
-% Generate a N-by-N matrix where A(i,j) = i + j;
-Aprealloc=zeros(N,N);
-for ii = 1:N
-     for jj = 1:N
-         Aprealloc(ii,jj) = ii + jj;
-     end
+function A = func2_loop_prealloc_rowmajor(N)
+    % (with preallocation)
+    % Generate a N-by-N matrix where A(i,j) = i + j;
+    A = zeros(N,N);
+    for ii = 1:N
+         for jj = 1:N
+             A(ii,jj) = ii + jj;
+         end
+    end
 end
-preallocate = toc;
 
-
-%Switch loop order so that we make best use of CPU cache
-tic
-% Generate a N-by-N matrix where A(i,j) = i + j;
-Aswitchloop=zeros(N,N);
-for jj = 1:N
-     for ii = 1:N
-         Aswitchloop(ii,jj) = ii + jj;
-     end
+function A = func3_loop_prealloc_colmajor(N)
+    % (Switch loop order so that we make best use of CPU cache)
+    % Generate a N-by-N matrix where A(i,j) = i + j;
+    A = zeros(N,N);
+    for jj = 1:N
+         for ii = 1:N
+             A(ii,jj) = ii + jj;
+         end
+    end
 end
-switchloop = toc;
 
-%Method 1: MESHGRID.
-tic
-[X, Y] = meshgrid(1:N, 1:N);
-Amesh = X + Y;
-mesh_time=toc;
+function A = func4_vectorized_meshgrid(N)
+    % Method 1: MESHGRID.
+    [X, Y] = meshgrid(1:N, 1:N);
+    A = X + Y;
+end
 
-% Method 2: Matrix multiplication.
-tic
-Amatmul = (1:N).' * ones(1, N) + ones(N, 1) * (1:N);
-matmul_time=toc;
+function A = func5_vectorized_matmult(N)
+    % Method 2: Matrix multiplication.
+    A = (1:N).' * ones(1, N) + ones(N, 1) * (1:N);
+end
 
-% Method 3: REPMAT.
-tic
-Arepmat = repmat(1:N, N, 1) + repmat((1:N).', 1, N);
-repmat_time=toc;
+function A = func6_vectorized_repmat(N)
+    % Method 3: REPMAT.
+    A = repmat(1:N, N, 1) + repmat((1:N).', 1, N);
+end
 
-% Method 4: CUMSUM.
-tic
-Acumsum = cumsum(ones(N)) + cumsum(ones(N), 2);
-cumsum_time=toc;
+function A = func7_vectorized_cumsum(N)
+    % Method 4: CUMSUM.
+    A = cumsum(ones(N)) + cumsum(ones(N), 2);
+end
 
-% Method 5: BSXFUN.
-tic
-Absxfun = bsxfun(@plus, 1:N, (1:N).');
-bsxfun_time=toc;
+function A = func8_vectorized_bsxfun(N)
+    % Method 5: BSXFUN.
+    A = bsxfun(@plus, 1:N, (1:N).');
+end
 
-fprintf('Original loop time is %f\n',original);
-fprintf('Preallocate and loop is %f\n',preallocate);
-fprintf('Switched loop order is %f\n',switchloop);
-fprintf('Meshgrid time is %f\n',mesh_time);
-fprintf('Matmul time is %f\n',matmul_time);
-fprintf('Repmat time is %f\n',repmat_time);
-fprintf('Cumsum time is %f\n',cumsum_time);
-fprintf('bsxfun time is %f\n',bsxfun_time);
+function A = func9_vectorized_hankel(N)
+    % HANKEL
+    A = hankel(2:N+1, N+1:2*N);
+end
 
-%test for equality
-test = all(all(repmat(Aloop,1,7) == [Amesh,Aprealloc,Aswitchloop,Amatmul,Arepmat,Acumsum,Absxfun]));
-
-if test==1
-    fprintf('\nAll results are equal\n');
-else
-    fprintf('\nAll results are not equal\n');
+function A = func10_mex_c(N)
+    % MEX-function
+    A = vectorization_mex_impl(N);
 end
